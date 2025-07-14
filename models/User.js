ecec3311c97de8f23c63e045cdb8be95bc9529fe
AUNG533 <aungkyawlinmon@gmail.com>
2025-07-14 13:45:33 +0700

    first commit

---
+++ models/User.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { unique } from "next/dist/build/utils";
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
+    cartItems: { type: Object, required: true },
+    password: { type: String, required: true },
+  },
+  { minimize: false }
+);
+
+const User = mongoose.models.User || mongoose.model("user", userSchema);
+
+export default User;
